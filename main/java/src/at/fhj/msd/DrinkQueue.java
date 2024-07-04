4436892931967d3ce642ce9207e6c6765f464bcf
Mihajlo Ilijic <mihajlo.ilijic@edu.fh-joanneum.at>
2024-07-04 13:35:45 +0200

    javadoc comments added to the src code

--- main/java/src/at/fhj/msd/DrinkQueue.java
+++ main/java/src/at/fhj/msd/DrinkQueue.java
@@ -1,8 +1,22 @@
+
+
+/**
+ * A generic queue implementation for objects that extend the Drink class.
+ *
+ * @param <T> the type of elements in the queue, which must extend Drink
+ */
+import java.util.ArrayList;
 class queue<T extends Drink> {
 
     int front = -1, rear = -1;
 
     ArrayList<T> A = new ArrayList<>();
+
+    /**
+     * Returns the front element of the queue without removing it.
+     *
+     * @return the front element of the queue, or null if the queue is empty
+     */
 
     T front()
     {
@@ -11,12 +25,22 @@ class queue<T extends Drink> {
 
         return A.get(front);
     }
+    /**
+     * Returns the rear element of the queue without removing it.
+     *
+     * @return the rear element of the queue, or null if the queue is empty
+     */
     T rear()
     {
         if (rear == -1)
             return null;
         return A.get(rear);
     }
+    /**
+     * Adds an element to the end of the queue.
+     *
+     * @param X the element to be added to the queue
+     */
     void enqueue(T X)
     {
         if (this.empty()) {
@@ -33,6 +57,9 @@ class queue<T extends Drink> {
                 A.add(X);
         }
     }
+    /**
+     * Removes the front element of the queue.
+     */
     void dequeue()
     {
 
@@ -56,6 +83,11 @@ class queue<T extends Drink> {
         }
     }
 
+    /**
+     * Checks if the queue is empty.
+     *
+     * @return true if the queue is empty, false otherwise
+     */
 
     boolean empty()
     {
@@ -64,6 +96,14 @@ class queue<T extends Drink> {
             return true;
         return false;
     }
+
+
+    /**
+     * Returns a string representation of the queue.
+     *
+     * @return a string representation of the queue
+     */
+
 
     public String toString()
     {
